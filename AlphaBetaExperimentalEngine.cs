e242af0179ae5bb4cd3c48120693fed4472e98bd
Peter Hojnoš <arowq26@gmail.com>
2020-03-07 01:39:53 +0100

    unfinished

--- AlphaBetaExperimentalEngine.cs
+++ AlphaBetaExperimentalEngine.cs
@@ -10,38 +10,56 @@ namespace FourInARow {
 
         protected override int eval(Game g) {
             List<int[]> tuplets = GetTuplets(g);
+            int[] goodPositions = GetGoodPositionCount(g);
+            int[] oddEvenBonuses = GetOddEvenBonuses(g);
             int[] positionBonuses = getPositionBonuses(g);
-            int[] oddEven = GetOddEven(g);
-            int result = (tuplets[0][0] - tuplets[0][1]) * 5 +
-                ((isFirstPlayer ? 1 : 100) * tuplets[1][0] - (isFirstPlayer ? 1 : 100) * tuplets[1][1]) * 10 +
-                ((isFirstPlayer ? 1 : 100) * tuplets[2][0] - (isFirstPlayer ? 1 : 100) * tuplets[2][1]) * 10 +
-                (oddEven[0] - oddEven[1]) +
-                ((isFirstPlayer ? 1 : 3) * positionBonuses[0] - (isFirstPlayer ? 1 : 3) * positionBonuses[1]);
-            return result;
-        }        
+            int getOver = -GetOver(g);
 
-        private int[] GetOddEven(Game g) {
+            int result = (tuplets[0][0] - (isFirstPlayer ? 1 : 1) * tuplets[0][1]) * 5 +
+                (tuplets[1][0] - (isFirstPlayer ? 10 : 100) * tuplets[1][1]) * 1000 +
+                (tuplets[2][0] - (isFirstPlayer ? 10 : 100) * tuplets[2][1]) * 100 +
+                (goodPositions[0] - goodPositions[1]) +
+                // getOver +
+                (oddEvenBonuses[0] - oddEvenBonuses[1]) * 1  +
+                (positionBonuses[0] - positionBonuses[1]) * 3;
+            return result;
+        }
+
+        private int[] GetOddEvenBonuses(Game g) {
             if (g.sizeY % 2 == 1) {
                 return new int[] { 0, 0 };
             }
-            int[] oddEvenCount = new int[2];
+            int[] oddEvenBonuses = new int[2];
             for (int i = 0; i < g.sizeX; i++) {
                 for (int j = 0; j < g.sizeY; j++) {
                     if (j % 2 == 0 && g.board[i, j] == true) {
-                        oddEvenCount[0]++;
+                        oddEvenBonuses[0]++;
+                        continue;
+                    }
+                    if (j % 2 == 1 && g.board[i, j] == true) {
+                        oddEvenBonuses[0]--;
+                        continue;
                     }
                     if (j % 2 == 0 && g.board[i, j] == false) {
-                        oddEvenCount[1]++;
+                        oddEvenBonuses[1]++;
+                        continue;
                     }
+                    if (j % 2 == 1 && g.board[i, j] == false) {
+                        oddEvenBonuses[1]--;
+                        continue;
+                    }
+                    //if (j == g.sizeY / 2 - 1 && g.board[i, j] == true) {
+                    //   oddEvenBonuses[0]++;
+                    //}
                 }
             }
-            return oddEvenCount;
+            return oddEvenBonuses;
         }
 
-        public List<int[]> GetTuplets(Game g) {            
+        public List<int[]> GetTuplets(Game g) {
             int[] triplets = new int[2];
-            int[] sureWin = new int[2];
-            int[] wins = new int[2];
+            int[] neighboringWinningPositionsCount = new int[2];
+            int[] quadruplets = new int[2];
             List<int[]> firstPlayerWiningPosition = new List<int[]>();
             List<int[]> secondPlayerWiningPosition = new List<int[]>();
 
@@ -59,7 +77,7 @@ namespace FourInARow {
                                 break;
                             if (g.board[i, j + k] == null)
                                 position = new int[] { i, j + k };
-                                hasBeenError = true;
+                            hasBeenError = true;
                             if (k == 3) {
                                 if (hasBeenError) {
                                     if (side) {
@@ -69,9 +87,9 @@ namespace FourInARow {
                                     }
                                     triplets[side ? 0 : 1]++;
                                 } else {
-                                    wins[side ? 0 : 1]++;
+                                    quadruplets[side ? 0 : 1]++;
                                 }
-                            } 
+                            }
                         }
 
                         // bottom left to upper right
@@ -94,7 +112,7 @@ namespace FourInARow {
                                     }
                                     triplets[side ? 0 : 1]++;
                                 } else {
-                                    wins[side ? 0 : 1]++;
+                                    quadruplets[side ? 0 : 1]++;
                                 }
                             }
                         }
@@ -119,7 +137,7 @@ namespace FourInARow {
                                     }
                                     triplets[side ? 0 : 1]++;
                                 } else {
-                                    wins[side ? 0 : 1]++;
+                                    quadruplets[side ? 0 : 1]++;
                                 }
                             }
                         }
@@ -144,40 +162,140 @@ namespace FourInARow {
                                     }
                                     triplets[side ? 0 : 1]++;
                                 } else {
-                                    wins[side ? 0 : 1]++;
+                                    quadruplets[side ? 0 : 1]++;
                                 }
                             }
                         }
                     }
                 }
             }
-
-            sureWin[0] = countSureWin(firstPlayerWiningPosition);
-            sureWin[1] = countSureWin(secondPlayerWiningPosition);
-            return new List<int[]> { triplets, sureWin, wins  };
+            quadruplets[1] += GetSureWinCount(g, secondPlayerWiningPosition);
+            neighboringWinningPositionsCount[0] = GetWiningSituationCount(firstPlayerWiningPosition);
+            neighboringWinningPositionsCount[1] = GetWiningSituationCount(secondPlayerWiningPosition);
+            return new List<int[]> { triplets, neighboringWinningPositionsCount, quadruplets };
         }
 
-        private int countSureWin(List<int[]> positions) {
+        private int GetWiningSituationCount(List<int[]> winningPositions) {
             int count = 0;
-            for (int i = 0; i < positions.Count - 1; i++) {
-                for (int j = i + 1; j < positions.Count; j++) {
-                    // two in row in y axis
-                    if (positions[i][0] == positions[j][0]) {
-                        if (positions[i][1] + 1 == positions[j][1] || positions[i][1] - 1 == positions[j][1]) {
+            for (int i = 0; i < winningPositions.Count - 1; i++) {
+                for (int j = i + 1; j < winningPositions.Count; j++) {
+                    // detect two neighboring winning positions in columns (y axis)
+                    if (winningPositions[i][0] == winningPositions[j][0]) {
+                        if (winningPositions[i][1] + 1 == winningPositions[j][1] || winningPositions[i][1] - 1 == winningPositions[j][1]) {
                             count++;
                         }
                     }
 
-                    // two in row in x axis
-                    if (positions[i][1] == positions[j][1]) {
-                        if(positions[i][0] + 1 == positions[j][0] || positions[i][0] - 1 == positions[j][0]) {
+                    // detect two neighboring winning positions in rows (x axis)
+                    if (winningPositions[i][1] == winningPositions[j][1]) {
+                        if (winningPositions[i][0] + 1 == winningPositions[j][0] || winningPositions[i][0] - 1 == winningPositions[j][0]) {
+                            count++;
+                        }
+                    }
+
+                    if (winningPositions[i][0] + 4 == winningPositions[j][0] || winningPositions[i][0] - 4 == winningPositions[j][0]) {
+                        // horizontally
+                        if (winningPositions[i][1] == winningPositions[j][1]) {
+                            count++;
+                        }
+
+                        // diagonally
+                        if (winningPositions[i][1] + 4 == winningPositions[j][1] || winningPositions[i][1] - 4 == winningPositions[j][1]) {
                             count++;
                         }
                     }
                 }
             }
             return count;
-        }       
+        }
+
+        private int GetSureWinCount(Game g, List<int[]> winningPositions) {
+            int count = 0;
+            for (int i = 0; i < winningPositions.Count; i++) {
+                if (winningPositions[i][1] == 0 || g.board[winningPositions[i][0], winningPositions[i][1] - 1] != null) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private int[] GetGoodPositionCount(Game g) {
+            int[] goodPositions = new int[2];
+            for (int s = 0; s < 2; s++) {
+                bool side = s % 2 == 0;
+                for (int i = 0; i < g.sizeX - 3; i++) {
+                    for (int j = 0; j < g.sizeY; j++) {
+                        if (g.board[i, j] == side) {
+                            bool myToken = false;
+                            for (int k = 1; k < 4; k++) {
+                                if (g.board[i + k, j] == !side) {
+                                    break;
+                                }
+                                if (g.board[i + k, j] == side) {
+                                    myToken = true;
+                                }
+                                if (k == 3 && myToken) {
+                                    goodPositions[side ? 0 : 1]++;
+                                }
+                            }
+
+                            myToken = false;
+                            for (int k = 1; k < 4; k++) {
+                                if (j + k >= g.sizeY) {
+                                    break;
+                                }
+                                if (g.board[i + k, j + k] == !side) {
+                                    break;
+                                }
+                                if (g.board[i + k, j + k] == side) {
+                                    myToken = true;
+                                }
+                                if (k == 3 && myToken) {
+                                    goodPositions[side ? 0 : 1]++;
+                                }
+                            }
+
+                            myToken = false;
+                            for (int k = 1; k < 4; k++) {
+                                if (j - k < 0) {
+                                    break;
+                                }
+                                if (g.board[i + k, j - k] == !side) {
+                                    break;
+                                }
+                                if (g.board[i + k, j - k] == side) {
+                                    myToken = true;
+                                }
+                                if (k == 3 && myToken) {
+                                    goodPositions[side ? 0 : 1]++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return goodPositions;
+        }
+
+        private int GetOver(Game g) {
+            int occupied = 0;
+            for (int i = 0; i < g.sizeX; i++) {
+                for (int j = 0; j < g.sizeY; j++) {
+                    if (g.board[i, j] != null) {
+                        occupied++;
+                    }
+                }
+            }
+            int over = 0;
+            for (int i = 0; i < g.sizeX; i++) {
+                for (int j = Math.Max(0, occupied / g.sizeX); j < g.sizeY; j++) {
+                    if (g.board[i, j] != null) {
+                        occupied++;
+                    }
+                }
+            }
+            return over;
+        }
 
         public override string getName() {
             return "Experimental " + base.getName() + " by Peter Hojnos";
